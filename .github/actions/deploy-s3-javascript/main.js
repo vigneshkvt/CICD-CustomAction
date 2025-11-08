6bbe133732f296e73eaf25bac4b5ce6d79fdb42f
vigneshkvt <vigneshbalavasan@outlook.com>
2025-11-08 11:12:33 +0530

    Add JS custom action and ignore action node_modules

---
+++ .github/actions/deploy-s3-javascript/main.js
@@ -0,0 +1,9 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const exec = require('@actions/exec');
+
+function run() {
+    core.info("This is a placeholder for S3 deployment logic.");
+}
+
+run();
